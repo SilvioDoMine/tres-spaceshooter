0ff8e4eff05623c289bb20918b71960e70a68340
SilvioDoMine <arthurhenrique@live.com>
2025-11-26 11:31:57 -0300

    silenciando erro

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+// default service worker file
+self.addEventListener('fetch', () => {});
+
+// You can add your custom service worker logic here
+// For example, caching strategies, push notifications, etc.
