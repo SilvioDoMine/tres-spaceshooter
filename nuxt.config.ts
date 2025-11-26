a9e53ad58ffdcbde3c5705684026919388ef7c35
SilvioDoMine <arthurhenrique@live.com>
2025-11-26 15:43:24 -0300

    Instalado pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@tresjs/nuxt',
-    '@nuxt/devtools'
-  ],
+  modules: ['@tresjs/nuxt', '@nuxt/devtools', '@pinia/nuxt'],
   compatibilityDate: '2025-11-01',
   css: ['./app/assets/css/main.css'],
   vite: {
