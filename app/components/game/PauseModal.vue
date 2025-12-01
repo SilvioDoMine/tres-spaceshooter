04a1436863f6a48d7ca0f13dc04a6e85ec08482a
SilvioDoMine <arthurhenrique@live.com>
2025-12-01 19:59:27 -0300

    Modal de pausa agora tem efeito

--- app/components/game/PauseModal.vue
+++ app/components/game/PauseModal.vue
@@ -5,7 +5,7 @@ const currentRunStore = useCurrentRunStore();
 </script>
 
 <template>
-    <div :class="currentRunStore.isPaused ? '' : 'hidden'" class="absolute z-50 inset-0 p-4 text-white bg-black/50">
+    <div :class="currentRunStore.isPaused ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'" class="absolute z-50 translate-all duration-500 inset-0 p-4 text-white bg-black/50">
         <div class="bg-gray-800 rounded-lg p-6 max-w-md mx-auto">
             <h2 class="text-2xl font-bold mb-4">Game Paused</h2>
             <p class="mb-6">The game is currently paused. You can resume or quit the game.</p>
