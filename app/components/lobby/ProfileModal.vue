462e2e8980a07c4fd44180ee0237fcd45b7c39ed
SilvioDoMine <arthurhenrique@live.com>
2025-12-22 22:36:20 -0300

    Ajuste de bug no ícone

--- app/components/lobby/ProfileModal.vue
+++ app/components/lobby/ProfileModal.vue
@@ -30,7 +30,7 @@ const changeNameModal = useModal('change-name-modal');
                     <!-- Stats -->
                     <!-- Tailwind for each child add a border b between them -->
                     <div class="bg-white/40 rounded-md p-2 flex flex-col flex-1">
-                        <div class="flex justify-between">
+                        <div class="flex justify-betwee items-center">
                             <h3 @click="changeNameModal.open()" class="cursor-pointer text-lg font-semibold text-amber-900">{{ lobbyStore.getCurrentProfileName }}</h3>
                             <svg @click="changeNameModal.open()" class="cursor-pointer text-amber-900 hover:text-amber-700 hover:scale-90 hover:rotate-360 transition-transform" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h6.525q.5 0 .75.313t.25.687t-.262.688T11.5 5H5v14h14v-6.525q0-.5.313-.75t.687-.25t.688.25t.312.75V19q0 .825-.587 1.413T19 21zm4-7v-2.425q0-.4.15-.763t.425-.637l8.6-8.6q.3-.3.675-.45t.75-.15q.4 0 .763.15t.662.45L22.425 3q.275.3.425.663T23 4.4t-.137.738t-.438.662l-8.6 8.6q-.275.275-.637.438t-.763.162H10q-.425 0-.712-.288T9 14m12.025-9.6l-1.4-1.4zM11 13h1.4l5.8-5.8l-.7-.7l-.725-.7L11 11.575zm6.5-6.5l-.725-.7zl.7.7z"/></svg>
                         </div>
